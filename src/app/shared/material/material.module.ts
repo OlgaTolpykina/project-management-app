b4ad7841902df01ef259b558c6a065006c92b4e4
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-04-26 17:04:47 +0300

    feat: add home-page component and header for unautorized user

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, MatInputModule],
+  exports: [MatInputModule],
 })
 export class MaterialModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from '@shared/material/material.module';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, MaterialModule],
+  exports: [MaterialModule],
 })
 export class SharedModule {}
