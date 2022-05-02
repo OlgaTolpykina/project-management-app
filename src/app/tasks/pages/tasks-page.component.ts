e62e42bef80ee8153494804710c3811756fb413b
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-05-02 14:02:28 +0300

    feat: add initial styles to tasks-page

--- src/app/tasks/pages/tasks-page.component.ts
+++ src/app/tasks/pages/tasks-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks-page.component.html',
   styleUrls: ['./tasks-page.component.scss'],
 })
-export class TasksPageComponent {}
+export class TasksPageComponent {
+  constructor(private route: ActivatedRoute) {}
+}
