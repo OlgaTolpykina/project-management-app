932349478301cc5d2fa2e15ec5016ea0e468b264
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-05-04 10:18:30 +0300

    feat: add layout and styles to task card

--- src/app/tasks/pipes/order-by.pipe.ts
+++ src/app/tasks/pipes/order-by.pipe.ts
@@ -1,13 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Column } from '@shared/types/column.model';
+import { Task } from '@shared/types/task.model';
 
 @Pipe({
   name: 'orderBy',
 })
 export class OrderByPipe implements PipeTransform {
-  transform(columns: Column[], sortValue: number): Column[] {
-    if (!sortValue) return columns;
+  transform<T extends Column | Task>(elements: T[], sortValue: number): T[] {
+    if (!sortValue) return elements;
 
-    return columns.sort((a, b) => (a.order > b.order ? 1 : -1));
+    return elements.sort((a, b) => (a.order > b.order ? 1 : -1));
   }
 }
