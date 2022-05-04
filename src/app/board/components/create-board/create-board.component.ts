889f54830a47560cb0918bfe5a41bbc35a8a1d88
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-05-04 17:19:56 +0300

    fix: small fixes

--- src/app/board/components/create-board/create-board.component.ts
+++ src/app/board/components/create-board/create-board.component.ts
@@ -15,12 +15,6 @@ export class CreateBoardComponent {
   constructor(private boardService: BoardService, private store: Store<AppState>) {}
 
   onCreateBoard(title: string) {
-    if (!localStorage.getItem('token')) {
-      const token =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTVmYmExMC00NmYwLTQyNjEtYjM5ZC05NjdiM2ExMTFlYTYiLCJsb2dpbiI6IkFseWFseWExNCIsImlhdCI6MTY1MTMxMTk1OX0.R-k_-jutBzjqsSc7P3QR3DYZ6ehkwVlNguh3_E-LfT4';
-      localStorage.setItem('token', token);
-    }
-
     this.boardService.createBoard({ title }).subscribe((data: Board | Error) => {
       if (!(data instanceof Error)) {
         this.store.dispatch(createNewBoard({ board: data as Board }));
