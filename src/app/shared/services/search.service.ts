93f553684f33a29228e67d7a12f782a569e7e3e1
Alexandr Karatkevich <87863536+koraleaxandr@users.noreply.github.com>
2022-05-12 16:25:16 +0300

    fix: temp save

--- src/app/shared/services/search.service.ts
+++ src/app/shared/services/search.service.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from '@app/redux/state.model';
 
 import { setSelectedBoardId, clearSelectedBoard } from '@app/redux/actions/board.actions';
+import { selectUsers } from '@app/redux/selectors/selectors';
 import { SearchObject } from '@shared/types/search-object.model';
 import { BoardService } from '@shared/services/board.service';
 import { ColumnService } from '@shared/services/column.service';
@@ -10,6 +11,7 @@ import { TaskService } from '@shared/services/task.service';
 import { UserAuthServiceService } from '@auth/services/user-auth-service.service';
 import { Task } from '@shared/types/task.model';
 import { Board } from '@shared/types/board.model';
+import { User } from '@shared/types/user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -28,6 +30,10 @@ export class SearchService {
   selectedTask: Task | null = null;
 
   isTaskRequestNeed: boolean = true;
+
+  openPage: boolean = false;
+
+  users: User[] = [];
 
   constructor(
     private boardService: BoardService,
@@ -66,7 +72,7 @@ export class SearchService {
         error: (err) => console.log('Error: ', err),
         complete: () => {
           if (i === 0) {
-            this.getSearchResult(this.searchString);
+            this.openPage = true;
           }
         },
       });
@@ -74,8 +80,9 @@ export class SearchService {
   }
 
   public async searchSubmit(): Promise<void> {
-    if (this.isTaskRequestNeed) {
+    if (this.isTaskRequestNeed || this.searchObject.tasks?.length === 0) {
       await this.getAllBoards();
+      this.getAllUsers();
     }
     console.log(this.searchString);
     console.log(this.searchObject);
@@ -84,19 +91,24 @@ export class SearchService {
       this.authService.getMessageForUser('signIn first', 'home');
     } else if (
       this.authService.isAuthorized === 'true' &&
-      this.searchString.length > MINIMAL_REQUEST_LENGTH
+      this.searchString.length > MINIMAL_REQUEST_LENGTH &&
+      this.openPage
     ) {
       this.getSearchResult(this.searchString);
+      this.authService.router.navigate(['search']);
     }
   }
 
   private getSearchResult(searchString: string) {
     this.filteredSearchResult = [];
     this.filteredSearchResult = this.searchObject.tasks!.filter((task) => {
-      const taskString = JSON.stringify(task);
-      return taskString.includes(searchString);
+      task.userName = this.users?.find((user) => user.id === task?.userId)?.name || '';
+      const taskString = task.title + task.description + task.userName + task.order.toString();
+      console.log(taskString);
+      console.log(taskString.includes(searchString));
+      return taskString.toLowerCase().includes(searchString.toLowerCase());
     });
-    this.authService.router.navigate(['search']);
+    console.log(this.filteredSearchResult);
   }
 
   public onSelect(task: Task) {
@@ -105,5 +117,15 @@ export class SearchService {
     this.store.dispatch(clearSelectedBoard());
     this.store.dispatch(setSelectedBoardId({ selectedBoardId }));
     this.authService.router.navigateByUrl('/b/' + selectedBoardId);
+  }
+
+  private getAllUsers() {
+    this.store
+      .select(selectUsers)
+      .pipe()
+      .subscribe((users) => {
+        this.users = users ? users : [];
+        console.log(users);
+      });
   }
 }
