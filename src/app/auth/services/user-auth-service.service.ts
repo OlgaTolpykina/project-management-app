7d922e7060c7961c92c6c50e6e69867cab4ce4d0
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-05-13 19:10:55 +0300

    fix: delete search functionality

--- src/app/auth/services/user-auth-service.service.ts
+++ src/app/auth/services/user-auth-service.service.ts
@@ -63,7 +63,6 @@ export class UserAuthServiceService {
           next: (users) => {
             if (!(users instanceof Error)) {
               this.users = users.length ? users : [];
-              console.log(this.users);
             }
           },
         });
--- src/app/shared/types/search-object.model.ts
+++
@@ -1,9 +0,0 @@
-import { Task } from './task.model';
-import { Board } from './board.model';
-import { Column } from './column.model';
-
-export interface SearchObject {
-  boards?: Board[];
-  columns?: Column[];
-  tasks?: Task[];
-}
