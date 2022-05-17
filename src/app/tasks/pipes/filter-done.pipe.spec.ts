a761e1fa356c0bdd1bc9e9c45ae11f8cac86b8b3
SergeySerzhan <s.sergey96@mail.ru>
2022-05-17 09:23:10 +0300

    fix: add all users select option

--- src/app/tasks/pipes/filter-done.pipe.spec.ts
+++ src/app/tasks/pipes/filter-done.pipe.spec.ts
@@ -3,6 +3,7 @@ import { FilterDonePipe } from './filter-done.pipe';
 describe('FilterDonePipe', () => {
   it('create an instance', () => {
     const pipe = new FilterDonePipe();
+
     expect(pipe).toBeTruthy();
   });
 });
