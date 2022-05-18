d5bc02aa73f5e6831c478248d26d22613208e886
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-05-18 14:30:13 +0300

    fix: remove unnecessary components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,7 +8,6 @@ import { HomePageComponent } from '@core/pages/home-page/home-page.component';
 import { SharedModule } from '@shared/shared.module';
 import { SidenavListComponent } from './components/sidenav-list/sidenav-list.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
-import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +16,6 @@ import { LoaderComponent } from './components/loader/loader.component';
     HomePageComponent,
     SidenavListComponent,
     CarouselComponent,
-    LoaderComponent,
   ],
   imports: [CommonModule, CoreRoutingModule, SharedModule],
   exports: [FooterComponent, HomePageComponent, HeaderComponent, SidenavListComponent],
